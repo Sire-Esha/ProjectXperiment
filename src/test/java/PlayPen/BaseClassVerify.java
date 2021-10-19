36da02a557dfe876d467a7a211df1a1b15980d23
Sire <siri.isha12@gmail.com>
2021-10-19 20:19:11 +1100

    Initial commit

---
+++ src/test/java/PlayPen/BaseClassVerify.java
@@ -0,0 +1,14 @@
+package PlayPen;
+
+import BaseClass.OpenBrowser;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class BaseClassVerify {
+    @Test
+    public void TestBClass() throws IOException {
+        OpenBrowser.NavToWebsite("url");
+        OpenBrowser.NavToWebsite("urlAccountScreen");
+    }
+}
